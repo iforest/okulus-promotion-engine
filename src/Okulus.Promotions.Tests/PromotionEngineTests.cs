01c80b5d568b66d72e25eb36fcab8381ba33d421
iforest <iforest@noserver.com>
2021-09-20 18:28:33 +0100

    PRM-8: Add Specification for the "Apply Promotions" User Story.

---
+++ src/Okulus.Promotions.Tests/PromotionEngineTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Okulus.Promotions.Tests
+{
+    public class PromotionEngineTests
+    {
+        private static IEnumerable<TestCaseData> promotionEngineTestCaseData = new[]
+        {
+            new TestCaseData(new[]{("A", 1), ("B", 1), ("C", 1)})
+                .Returns(100)
+                .SetName("Scenario A"),
+            new TestCaseData(new[]{("A", 5), ("B", 5), ("C", 1)})
+                .Returns(370)
+                .SetName("Scenario B"),
+            new TestCaseData(new[]{("A", 3), ("B", 5), ("C", 1), ("D", 1)})
+                .Returns(280)
+                .SetName("Scenario C"),
+        };
+
+        [TestCaseSource(nameof(promotionEngineTestCaseData))]
+        public decimal ShouldCalculateTotalOrderValueAfterApplyingActivePromotion(
+            (string skuId, int quantity)[] items
+        )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
